ea611624573385e6a22ba177f88cf7b52a7225ad
Aradia D Arriaga <aradia@myfo.tech>
2022-10-28 20:40:51 -0500

    feat: Add simple HTML page

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path')
+const express = require('express')
+const app = express()
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', function (req, res) {
+    const html = fs.readFileSync('./src/index.html', 'utf8')
+    res.send(html)
+})
+
+app.get('/cheatSheet', function (req, res) {
+    const html = fs.readFileSync('./src/cheatSheet.html', 'utf8')
+    res.send(html)
+})
+
+app.listen(3000)
